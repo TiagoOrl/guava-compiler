501ccb2210bc8520774e637e9e549dc5c82c097a
tiagoorl <tiago_orlando@yahoo.com>
2025-02-05 21:57:25 -0300

    feat(parser): implemented scope functions

--- scope.c
+++ scope.c
@@ -83,10 +83,69 @@ void* scope_iterate_back(struct scope* scope)
 }
 
 
-void* scope_last_entity_of_scope(struct scope* scope) 
+void* scope_last_entity_at_scope(struct scope* scope) 
 {
     if(vector_count(scope->entities) == 0)
         return NULL;
 
     return vector_back_ptr(scope->entities);
+}
+
+
+void* scope_last_entity_from_scope_stop_at(struct scope* scope, struct scope* stop_scope)
+{
+    if(scope == stop_scope)
+    {
+        return NULL;
+    }
+
+    void* last = scope_last_entity_at_scope(scope);
+
+    if (last)
+        return last;
+
+    struct scope* parent = scope->parent;
+
+    if (parent)
+        return scope_last_entity_from_scope_stop_at(parent, stop_scope);
+
+    return NULL;
+}
+
+
+void* scope_last_entity_stop_at(struct compile_process* process, struct scope* stop_scope)
+{
+    return scope_last_entity_from_scope_stop_at(process->scope.current, stop_scope);
+}
+
+
+void* scope_last_entity(struct compile_process* process)
+{
+    return scope_last_entity_stop_at(process, NULL);
+}
+
+
+void scope_push(struct compile_process* process, void* ptr, size_t elem_size)
+{
+    vector_push(process->scope.current->entities, &ptr);
+    process->scope.current->size += elem_size;
+}
+
+
+void scope_scope_finish(struct compile_process* process)
+{
+    struct scope* new_current_scope = process->scope.current->parent;
+    scope_dealloc(process->scope.current);
+    process->scope.current = new_current_scope;
+
+    if (process->scope.root && !process->scope.current)
+    {
+        process->scope.root = NULL;
+    }
+}
+
+
+struct scope* scope_current(struct compile_process* process)
+{
+    return process->scope.current;
 }
