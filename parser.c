a9d42d23155bd5622f8de9e505b113e32285f72e
tiagoorl <tiago_orlando@yahoo.com>
2025-02-18 19:58:47 -0300

    feat(parser): pushing variables to the scope

--- parser.c
+++ parser.c
@@ -96,9 +96,9 @@ struct parser_scope_entity* parser_scope_last_entity()
 }
 
 
-void parser_scope_push(struct node* node, size_t size)
+void parser_scope_push(struct parser_scope_entity* entity, size_t size)
 {
-    scope_push(current_process, node, size);
+    scope_push(current_process, entity, size);
 }
 
 
@@ -715,7 +715,10 @@ void make_variable_node_and_register(
     // Calculate the scope offset
     parser_scope_offset(var_node, history);
     // Push the variable node to the scope
-
+    parser_scope_push(
+        parser_new_scope_entity(var_node, var_node->var.aoffset, 0), 
+        var_node->var.type.size
+    );
     node_push(var_node);
 }    
 
